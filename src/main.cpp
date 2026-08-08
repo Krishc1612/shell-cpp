68c3629ff695054a258f870adb812a0fe5a579af
Krish <krishc4005@gmail.com>
2026-08-09 03:34:10 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -21,31 +21,6 @@ unordered_map<string, function<void(vector<string>)>> commands = {
 	{"cd", cd}
 };
 
-// pair<bool, string> hasValidExecutable(string& PATH, string& command){
-// 	string envVar = "";
-
-// 	for (int i = 0; i < PATH.size(); i++){
-// 		if (PATH[i] == ':'){
-// 			envVar += "/" + command;
-// 			path p = envVar;
-
-// 			if (exists(p) && is_regular_file(p)){
-// 				auto perm = status(p).permissions();
-// 				bool executable = ((perm & perms::owner_exec) != perms::none);
-
-// 				if (executable) {
-// 					return {true, envVar};
-// 				}
-// 			}
-
-// 			envVar = "";
-// 		}
-// 		else envVar += PATH[i];
-// 	}
-
-// 	return {false, ""};
-// }
-
 vector<string> parse(string& input){
 	vector<string> args;
 	string arg = "";
@@ -69,7 +44,6 @@ int main() {
 	std::cout << std::unitbuf;
 	std::cerr << std::unitbuf;
 
-	// TODO: Uncomment the code below to pass the first stage
 	while (true){
 		cout << "$ ";
 
@@ -113,80 +87,6 @@ int main() {
 			}
 		}
 	}
-
-	// 	if (input == "exit") {
-	// 		break;
-	// 	}
-	// 	else if (input.substr(0, 5) == "echo "){
-	// 		cout << input.substr(5, input.size()) << endl;
-	// 	}
-	// 	else if (input.substr(0, 5) == "type "){
-	// 		string command = input.substr(5);
-
-	// 		if (builtins.find(command) != builtins.end()){
-	// 			cout << command << " is a shell builtin" << endl;
-	// 			continue;
-	// 		}
-
-	// 		string PATH = getenv("PATH");
-	// 		PATH += ":";
-
-	// 		auto [isValid, pathTo] = hasValidExecutable(PATH, command);
-
-	// 		if (isValid){
-	// 			cout << command << " is " << pathTo << endl;
-	// 		}
-	// 		else {
-	// 			cout << command << ": not found" << endl;
-	// 		}
-	// 	}
-	// 	else if (input == "pwd"){
-	// 		char path[1024];
-	// 		getcwd(path, sizeof(path));
-
-	// 		cout << path << endl;
-	// 	}
-	// 	else {
-	// 		vector<string> params;
-	// 		string arg;
-
-	// 		for (int i = 0; i < input.size(); i++){
-	// 			if (input[i] != ' ') arg += input[i];
-	// 			else {
-	// 				params.push_back(arg);
-	// 				arg = "";
-	// 			}
-	// 		}
-	// 		if (arg != "") params.push_back(arg);
-
-	// 		string command = params[0];
-
-	// 		string PATH = getenv("PATH");
-	// 		PATH += ":";
-
-	// 		auto [isValid, pathTo] = hasValidExecutable(PATH, command);
-
-	// 		if (isValid){
-	// 			if (fork() == 0){
-	// 				const char* p = pathTo.data();
-	// 				vector<char*> args;
-
-	// 				for (auto& param: params){
-	// 					args.push_back(param.data());
-	// 				}
-	// 				args.push_back(nullptr);
-
-	// 				execv(p, args.data());
-	// 			}
-	// 			else {
-	// 				wait(nullptr);
-	// 			}
-	// 		}
-	// 		else {
-	// 			cout << command << ": not found" << endl;
-	// 		}
-	// 	}
-	// }
 
 	return 0;
 }
