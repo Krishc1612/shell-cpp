45545baa7c0447d8a45b8788076ab0da77c62d97
Krish <krishc4005@gmail.com>
2026-08-29 15:47:47 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,34 @@
 #include "tokenize.h"
 #include "parse.h"
 #include "execute.h"
+#include <readline/readline.h>
 
 using namespace std;
+
+char** command_completion(const char*, int, int);
+char* command_generator(const char*, int);
+
+const char* completions[] = {
+	"exit",
+	"echo",
+	NULL
+};
 
 int main() {
   // Flush after every std::cout / std:cerr
 	std::cout << std::unitbuf;
 	std::cerr << std::unitbuf;
 
-	while (true){
-		cout << "$ ";
+	rl_attempted_completion_function = command_completion;
 
+	while (true){
 		string input;
-		getline(cin, input);
+		char* buffer = readline("$ ");
+
+		if (buffer){
+			input = buffer;
+			free(buffer);
+		}
 
 		vector<Token> tokens = tokenize(input);
 
@@ -29,4 +44,27 @@ int main() {
 	}
 
 	return 0;
+}
+
+char** command_completion(const char* text, int start, int end){
+	rl_attempted_completion_over = 1;
+	return rl_completion_matches(text, command_generator);
+}
+
+char* command_generator(const char* text, int state){
+	static int list_idx, len;
+	const char* command;
+
+	if(!state){
+		list_idx = 0;
+		len = strlen(text);
+	}
+
+	while ((command = completions[list_idx++])){
+		if (strncmp(command, text, len) == 0){
+			return strdup(command);
+		}
+	}
+
+	return NULL;
 }
